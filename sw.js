9158da07f9e94a45272de544b3fbf257ebcca417
Andrew zhang <andrewzhang@AndrewdeMacBook-Pro.local>
2026-06-18 18:11:41 +0800

    fix: bump SW cache version for replay button

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
+// v2 - add replay button
 self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",()=>caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request)))
