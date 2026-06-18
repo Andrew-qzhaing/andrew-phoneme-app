6f09fa0c4898890d47b5cb7369092eb6aacbe5b6
Andrew zhang <andrewzhang@AndrewdeMacBook-Pro.local>
2026-06-18 09:03:32 +0800

    fix: disable SW caching (was serving stale broken pages)

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-const CACHE = 'phoneme48-disabled-v2';
-const ASSETS = [];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener("install",()=>self.skipWaiting());self.addEventListener("activate",()=>caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request)))
